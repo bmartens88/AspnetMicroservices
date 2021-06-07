2b3f026227fef3dde7ea7d15f35b3c6f8a623d21
Bas Martens <bmartens1988@kpnmail.nl>
2021-06-07 16:29:47 +0200

    Implementing CQRS/DDD/Clean Architecture

---
+++ src/Services/Ordering/Ordering.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+using System.Reflection;
+
+namespace Ordering.Application
+{
+    public static class ApplicationServicesRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services) =>
+            services.AddAutoMapper(Assembly.GetExecutingAssembly())
+                .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly())
+                .AddMediatR(Assembly.GetExecutingAssembly())
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>))
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+    }
+}
