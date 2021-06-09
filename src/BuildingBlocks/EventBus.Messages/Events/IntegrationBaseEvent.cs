94eed774a073d462e5022752919a070dc76230fd
Bas Martens <bmartens1988@kpnmail.nl>
2021-06-09 13:29:32 +0200

    Moved Discount.API project, added RabbitMQ image

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EventBus.Messages.Events
+{
+    public abstract class IntegrationBaseEvent
+    {
+        public Guid Id { get; }
+        public DateTime CreationDate { get; }
+
+        protected IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        protected IntegrationBaseEvent(Guid id, DateTime createDate)
+        {
+            Id = id;
+            CreationDate = createDate;
+        }
+    }
+}
