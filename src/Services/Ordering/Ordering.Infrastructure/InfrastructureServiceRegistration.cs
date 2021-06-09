3f7cddf8f169f18c05a8369f9cc7cd771dc974c1
Bas Martens <bmartens1988@kpnmail.nl>
2021-06-09 09:03:06 +0200

    Finished development of Ordering.API and other layers

---
+++ src/Services/Ordering/Ordering.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Infrastructure;
+using Ordering.Application.Models;
+using Ordering.Infrastructure.Mail;
+using Ordering.Infrastructure.Persistence;
+using Ordering.Infrastructure.Repositories;
+
+namespace Ordering.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration) =>
+            services
+                .AddDbContext<OrderContext>(options =>
+                    options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")))
+                .AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>))
+                .AddScoped<IOrderRepository, OrderRepository>()
+                .Configure<EmailSettings>(_ => configuration.GetSection("EmailSettings"))
+                .AddTransient<IEmailService, EmailService>();
+    }
+}
